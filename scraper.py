6a0a8b0ca2e19b0529bb30b21fa0bf6334394ab6
William Zhen <wzhen6@uci.edu>
2024-04-15 18:49:25 -0700

    testing

--- scraper.py
+++ scraper.py
@@ -6,6 +6,8 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    for i in range(0, 1):
+        print("hello")
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
